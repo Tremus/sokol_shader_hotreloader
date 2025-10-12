fc2aa46f883d409fe06f2b9a459b262ae1221fe0
Trémus <19359682+Tremus@users.noreply.github.com>
2025-10-12 14:58:06 +1000

    Improve defaults and documentation

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ CLI tool for hotreloading when editing shaders.
 Intended for use with `sokol_gfx`.
 
 Watches a directory for changes to `.glsl` code, then compiles them with `sokol-shdc`. `sokol-shdc` will generate a `.h` file, then presumably your C/C++ hotreloader detects this and recompiles/hotreloads your program.
+
+```
+Usage: {PROGRAM_NAME} [-i watch_dir] [-o out_dir] [-l lang]
+
+Command line tool to compile shaders written for sokol-shdc as C header filess
+(.h). To learn more about sokol-shdc, visit this URL:
+https://github.com/floooh/sokol-tools/blob/master/docs/sokol-shdc.md
+
+Shaders are required to use the ".glsl" extension, and the generated
+headers use the suffix/extension ".glsl.h"
+
+Options:
+
+  -i/-w     Input/watch directory. Defaults to your current working directory
+  -o        Output directory. Defaults generating headers in the same
+            directory as the shader.
+  -l        Shader language. Use the same options as with sokol-shdc
+            eg. glsl430:wgsl:hlsl5:metal_macos:...etc
+            Defaults to using metal_macos on macOS, and hlsl5 on Windows
+
+All arguments are optional.
+```
--- main.c
+++ main.c
@@ -3,94 +3,47 @@
 #include <xhl/debug.h>
 #include <xhl/files.h>
 
-enum
-{
-    LANG_GLSL410,
-    LANG_GLSL430,
-    LANG_GLSL300ES,
-    LANG_HLSL4,
-    LANG_HLSL5,
-    LANG_METAL_MACOS,
-    LANG_METAL_IOS,
-    LANG_METAL_SIM,
-    LANG_WGSL,
-    LANG_COUNT,
-};
-static const char* LANG_NAMES[] = {
-    "glsl410",
-    "glsl430",
-    "glsl300es",
-    "hlsl4",
-    "hlsl5",
-    "metal_macos",
-    "metal_ios",
-    "metal_sim",
-    "wgsl",
-};
-
 #if defined(_WIN32)
 const char* DEFAULT_LANG = "hlsl5";
 #elif defined(__APPLE__)
 const char* DEFAULT_LANG = "metal_macos";
 #endif
 
-/*
-Target shader languages (used with -l --slang):
-  - glsl410        desktop OpenGL backend (SOKOL_GLCORE)
-  - glsl430        desktop OpenGL backend (SOKOL_GLCORE)
-  - glsl300es      OpenGLES3 and WebGL2 (SOKOL_GLES3)
-  - hlsl4          Direct3D11 with HLSL4 (SOKOL_D3D11)
-  - hlsl5          Direct3D11 with HLSL5 (SOKOL_D3D11)
-  - metal_macos    Metal on macOS (SOKOL_METAL)
-  - metal_ios      Metal on iOS devices (SOKOL_METAL)
-  - metal_sim      Metal on iOS simulator (SOKOL_METAL)
-  - wgsl           WebGPU (SOKOL_WGPU)
+struct Context
+{
+    char path_cwd[1024];
+    char path_watch[1024];
+    char path_output[1024];
 
-  Output formats (used with -f --format):
-  - sokol          C header which includes both decl and inlined impl
-  - sokol_impl     C header with STB-style SOKOL_SHDC_IMPL wrapped impl
-  - sokol_zig      Zig module file
-  - sokol_nim      Nim module file
-  - sokol_odin     Odin module file
-  - sokol_rust     Rust module file
-  - sokol_d        D module file
-  - sokol_jai      Jai module file
-  - sokol_c3       C3 module file
-  - bare           raw output of SPIRV-Cross compiler, in text or binary format
-  - bare_yaml      like bare, but with reflection file in YAML format
+    const char* arg_watch;
+    const char* arg_output;
+    const char* arg_lang;
 
-  Options:
+    char cmdbuf[2048];
+};
 
--h --help                           - print this help text
--i --input=<GLSL file>              - input source file
--o --output=<C header>              - output source file
--l --slang=<glsl430:glsl300es...>   - output shader language(s), see above for list
-   --defines=<define1:define2...>   - optional preprocessor defines
--m --module=<(null)>                - optional @module name override
--r --reflection                     - generate runtime reflection functions
--b --bytecode                       - output bytecode (HLSL and Metal)
--f --format=<[sokol|sokol_impl|sokol_zig|sokol_nim|sokol_odin|soko - output format (default: sokol)
--e --errfmt=<[gcc|msvc]>            - error message format (default: gcc)
--d --dump                           - dump debugging information to stderr
--g --genver=<[int]>                 - version-stamp for code-generation
--t --tmpdir=<[dir]>                 - directory for temporary files (use output dir if not specified)
-   --ifdef                          - wrap backend-specific generated code in #ifdef/#endif
--n --noifdef                        - obsolete, superseded by --ifdef
-   --save-intermediate-spirv        - save intermediate SPIRV bytecode (for debug inspection)
-  */
+int  g_running = 1;
+void ctrl_c_callback(int code)
+{
+    fprintf(stderr, "Terminating\n");
+    g_running = 0;
+}
+
+// ARG PARSING
+int find_key_idx(const char* key, int argc, char* argv[])
+{
+    for (int i = 0; i < argc; i++)
+        if (0 == strcmp(key, argv[i]))
+            return i;
+    return -1;
+}
+bool has_key(const char* key, int argc, char* argv[]) { return find_key_idx(key, argc, argv) >= 0; }
 
 char* find_value(const char* key, int argc, char* argv[])
 {
-    for (int i = 0; i < argc; i++)
-    {
-        if (strcmp(key, argv[i]))
-        {
-            if (i + 1 < argc)
-            {
-                return argv[i + 1];
-            }
-        }
-    }
+    int key_idx = find_key_idx(key, argc, argv);
+    if (key_idx >= 0 && key_idx + 1 < argc)
+        return argv[key_idx + 1];
     return NULL;
 }
 
@@ -102,57 +55,56 @@ bool begins_with(const char* str, const char* prefix)
     return i > 0 && prefix[i] == 0;
 }
 
-struct Context
-{
-    char path_cwd[1024];
-    char path_watch[1024];
-    char path_output[1024];
-
-    const char* arg_watch;
-    const char* arg_output;
-    const char* arg_lang;
-};
-
-int  g_running = 1;
-void ctrl_c_callback(int code)
-{
-    fprintf(stderr, "Terminating\n");
-    g_running = 0;
-}
-
 int on_change(enum XFILES_WATCH_TYPE type, const char* path, void* udata)
 {
     if (type == XFILES_WATCH_CREATED || type == XFILES_WATCH_MODIFIED)
     {
-        printf("Checking: %s\n", path);
-
         const char* ext = xfiles_get_extension(path);
-        if (strcmp(ext, ".glsl")) // looks like a shader
+        if (strcmp(ext, ".glsl") == 0) // looks like a shader
         {
-            struct Context* ctx           = udata;
-            const char*     name          = xfiles_get_name(path);
-            char            outpath[1024] = {0};
-            snprintf(outpath, sizeof(outpath), "%s" XFILES_DIR_STR "%s.h", ctx->path_output, name);
+            const char* label = type == XFILES_WATCH_CREATED ? "New File" : "Changed";
+            printf("%s: %s\n", label, path);
 
-            char cmdbuf[2048];
+            struct Context* ctx  = udata;
+            const char*     name = xfiles_get_name(path);
 
             // sokol-shdc -i {INPATH} -o {OUTPATH} -l {LANG}
-            snprintf(
-                cmdbuf,
-                sizeof(cmdbuf),
-                "sokol-shdc -i %s -o %s" XFILES_DIR_STR "%s.h -l %s",
-                path,
-                ctx->path_output,
-                name,
-                ctx->arg_lang);
-            printf("Running command: %s\n", cmdbuf);
+            if (ctx->arg_output != NULL) // output dir was specified
+            {
+                snprintf(
+                    ctx->cmdbuf,
+                    sizeof(ctx->cmdbuf),
+                    "sokol-shdc -i %s -o %s" XFILES_DIR_STR "%s.h -l %s",
+                    path,
+                    ctx->path_output,
+                    name,
+                    ctx->arg_lang);
+            }
+            else // if (ctx->arg_output == NULL) // no output dir was specified.
+            {
+                // Default to generating header in same dir as shader
+                snprintf(ctx->cmdbuf, sizeof(ctx->cmdbuf), "sokol-shdc -i %s -o %s.h -l %s", path, path, ctx->arg_lang);
+            }
+            printf("Running command: %s\n", ctx->cmdbuf);
             fflush(stdout);
 
 #ifdef _WIN32
 #error "TODO: use different implementation for windows in order to capture stdout"
 #else
-            int code = system(cmdbuf);
+            int code = system(ctx->cmdbuf);
 #endif
+            if (code) // non-zero error
+            {
+                printf("[ERROR] Returned with code: %d\n", code);
+            }
+            else // (code == 0)
+            {
+                if (ctx->arg_output != NULL)
+                    printf("Generated: %s" XFILES_DIR_STR "%s.h\n", ctx->path_output, name);
+                else
+                    printf("Generated: %s.h\n", path);
+            }
+            fflush(stdout);
         }
     }
     return g_running;
@@ -160,6 +112,35 @@ int on_change(enum XFILES_WATCH_TYPE type, const char* path, void* udata)
 
 int main(int argc, char* argv[])
 {
+    if (has_key("-h", argc, argv) || has_key("--help", argc, argv))
+    {
+        const char* executable_name = xfiles_get_name(argv[0]);
+        printf(
+            "\n"
+            "Usage: %s [-i watch_dir] [-o out_dir] [-l lang]\n"
+            "\n"
+            "Command line tool to compile shaders written for sokol-shdc as C header files\n"
+            "(.h). To learn more about sokol-shdc, visit this URL:\n"
+            "https://github.com/floooh/sokol-tools/blob/master/docs/sokol-shdc.md\n"
+            "\n"
+            "Shaders are required to use the \".glsl\" extension, and the generated headers\n"
+            "use the suffix/extension \".glsl.h\"\n"
+            "\n"
+            "Options:\n"
+            "\n"
+            "  -i/-w     Input/watch directory. Defaults to your current working directory\n"
+            "  -o        Output directory. Defaults generating headers in the same\n"
+            "            directory as the shader.\n"
+            "  -l        Shader language. Use the same options as with sokol-shdc\n"
+            "            eg. glsl430:wgsl:hlsl5:metal_macos:...etc\n"
+            "            Defaults to using metal_macos on macOS, and hlsl5 on Windows\n"
+            "\n"
+            "All arguments are optional.\n"
+            "\n",
+            executable_name);
+        return 0;
+    }
+
     struct Context ctx = {0};
 #ifdef _WIN32
 // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/getcwd-wgetcwd?view=msvc-170
@@ -168,21 +149,22 @@ int main(int argc, char* argv[])
     getcwd(ctx.path_cwd, sizeof(ctx.path_cwd));
 #endif
 
-    ctx.arg_watch  = find_value("-i", argc, argv);
+    ctx.arg_watch = find_value("-i", argc, argv);
+    if (!ctx.arg_watch)
+        ctx.arg_watch = find_value("-w", argc, argv);
     ctx.arg_output = find_value("-o", argc, argv);
     ctx.arg_lang   = find_value("-l", argc, argv);
 
     if (!ctx.arg_lang)
         ctx.arg_lang = DEFAULT_LANG;
-    if (!ctx.arg_watch)
-        ctx.arg_watch = ctx.path_cwd;
-    if (!ctx.arg_output)
-        ctx.arg_output = ctx.arg_watch;
 
     for (int i = 0; i < 2; i++)
     {
         const char* in  = i == 0 ? ctx.arg_watch : ctx.arg_output;
         char*       out = i == 0 ? ctx.path_watch : ctx.path_output;
+
+        if (!in)
+            continue;
 
         if (!xfiles_exists(in))
         {
@@ -197,7 +179,7 @@ int main(int argc, char* argv[])
         {
             fprintf(stderr, "Invalid path: %s\n", in);
         }
-        // Remove trailing
+        // Remove trailing '/' || '\\'
         if (in[inlen - 1] == XFILES_DIR_CHAR)
             inlen--;
 
@@ -207,19 +189,20 @@ int main(int argc, char* argv[])
 
         pathlen += snprintf(out + pathlen, sizeof(ctx.path_output) - pathlen, "%.*s", inlen, in);
     }
-    printf("Watch path: %s\n", ctx.path_watch);
-    printf("Output path: %s\n", ctx.path_output);
+    if (ctx.arg_watch == NULL)
+        memcpy(ctx.path_watch, ctx.path_cwd, sizeof(ctx.path_cwd));
+
+    printf("Watching directory: %s\n", ctx.path_watch);
+    if (ctx.arg_output)
+        printf("Output path: %s\n", ctx.path_output);
+    else
+        printf("Output path: Same directory as source shader\n");
 
     printf("Press Crtl+C to exit\n");
-    g_running = 1;
     signal(SIGINT, ctrl_c_callback);
     fflush(stdout);
 
     xfiles_watch(ctx.path_watch, 50, &ctx, on_change);
 
-    return 0;
-
-usage:
-    printf("Usage: shader-watcher [-i watch_dir] [-o out_dir] [-l lang]\n");
     return 0;
 }
