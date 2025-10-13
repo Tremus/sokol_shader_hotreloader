d45bf23f5e9388dd6005d69a388931ed8cb85a12
Tré Dudman <19359682+Tremus@users.noreply.github.com>
2025-10-13 11:32:43 +1000

    Add Windows implementation

--- main.c
+++ main.c
@@ -1,13 +1,32 @@
 #define XHL_FILES_IMPL
 
-#include <xhl/debug.h>
-#include <xhl/files.h>
+#ifdef NDEBUG
+#define XFILES_ASSERT(cond)                                                                                            \
+    if (!(cond))                                                                                                       \
+    {                                                                                                                  \
+        fprintf(stderr, "[Error]: %s - %s:%d" #cond, __FUNCTION__, __LINE__);                                          \
+        exit(1);                                                                                                       \
+    }
+#endif
 
 #if defined(_WIN32)
+
+#define _CRT_SECURE_NO_WARNINGS
+#define UNICODE
+#define _UNICODE
+#include <wchar.h>
 const char* DEFAULT_LANG = "hlsl5";
+
 #elif defined(__APPLE__)
+
 const char* DEFAULT_LANG = "metal_macos";
-#endif
+
+#endif // win/mac
+
+#include <signal.h>
+#include <stdio.h>
+#include <xhl/debug.h>
+#include <xhl/files.h>
 
 struct Context
 {
@@ -89,15 +108,80 @@ int on_change(enum XFILES_WATCH_TYPE type, const char* path, void* udata)
             fflush(stdout);
 
 #ifdef _WIN32
-#error "TODO: use different implementation for windows in order to capture stdout"
+            // Using 'system()' to call our build command is way simpler, but creates some stdout buffering problems...
+            // Windows prefer that you use CreateProcessW.
+            // https://learn.microsoft.com/en-us/windows/win32/procthread/creating-processes
+            // https://learn.microsoft.com/en-au/windows/win32/procthread/creating-a-child-process-with-redirected-input-and-output?redirectedfrom=MSDN
+            STARTUPINFO         si;
+            PROCESS_INFORMATION pi;
+            SECURITY_ATTRIBUTES sa;
+            BOOL                ok = 0;
+            memset(&si, 0, sizeof(si));
+            memset(&pi, 0, sizeof(pi));
+            memset(&sa, 0, sizeof(sa));
+
+            sa.nLength              = sizeof(sa);
+            sa.bInheritHandle       = TRUE;
+            sa.lpSecurityDescriptor = NULL;
+
+            HANDLE hChildStdoutRd, hChildStdoutWr;
+            BOOL   CreatePipe_Result = CreatePipe(&hChildStdoutRd, &hChildStdoutWr, &sa, 0);
+            XFILES_ASSERT(CreatePipe_Result);
+            BOOL SetHandleInformation_Result = SetHandleInformation(hChildStdoutRd, HANDLE_FLAG_INHERIT, 0);
+            XFILES_ASSERT(SetHandleInformation_Result);
+
+            si.cb          = sizeof(si);
+            si.dwFlags    |= STARTF_USESHOWWINDOW; // Stops a terminal window popping up as it runs the command
+            si.hStdError   = hChildStdoutWr;
+            si.hStdOutput  = hChildStdoutWr;
+            si.dwFlags    |= STARTF_USESTDHANDLES; // Lets us use the stdout pipe
+
+            // Run build command in child process.
+            WCHAR wcmdbuf[XFILES_ARRLEN(ctx->cmdbuf)] = {0};
+            int   MultiByteToWideChar_Result =
+                MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, ctx->cmdbuf, -1, wcmdbuf, XFILES_ARRLEN(wcmdbuf));
+            XFILES_ASSERT(MultiByteToWideChar_Result);
+            BOOL CreateProcessW_Result = CreateProcessW(0, wcmdbuf, NULL, NULL, TRUE, 0, NULL, NULL, &si, &pi);
+            XFILES_ASSERT(CreateProcessW_Result);
+
+            // Wait until child process exits
+            // https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
+            WaitForSingleObject(pi.hProcess, 1000); // 1sec. Shouldn't take longer
+
+#define EXIT_CODE_FORMAT "%lu"
+            DWORD code = 0;
+            // https://learn.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-getexitcodeprocess
+            GetExitCodeProcess(pi.hProcess, &code);
+
+            // Cleanup process
+            CloseHandle(hChildStdoutWr);
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
+
+            char   buffer[4096]  = {0};
+            DWORD  bytesRead     = 0;
+            DWORD  bytesWritten  = 0;
+            HANDLE hParentStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            for (;;)
+            {
+                ok = ReadFile(hChildStdoutRd, buffer, sizeof(buffer), &bytesRead, NULL);
+                if (!ok || bytesRead == 0)
+                    break;
+
+                ok = WriteFile(hParentStdOut, buffer, bytesRead, &bytesWritten, NULL);
+                if (!ok)
+                    break;
+            }
 #else
+#define EXIT_CODE_FORMAT "%d"
             int code = system(ctx->cmdbuf);
 #endif
             if (code) // non-zero error
             {
-                printf("[ERROR] Returned with code: %d\n", code);
+                printf("[ERROR] Returned with code: " EXIT_CODE_FORMAT "\n", code);
             }
-            else // (code == 0)
+            else // (code == 0) // Success!
             {
                 if (ctx->arg_output != NULL)
                     printf("Generated: %s" XFILES_DIR_STR "%s.h\n", ctx->path_output, name);
@@ -143,8 +227,23 @@ int main(int argc, char* argv[])
 
     struct Context ctx = {0};
 #ifdef _WIN32
-// https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/getcwd-wgetcwd?view=msvc-170
-#error "TODO"
+    {
+        // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/getcwd-wgetcwd?view=msvc-170
+        wchar_t  wcwd[MAX_PATH];
+        wchar_t* _wgetcwd_ret = _wgetcwd(wcwd, XFILES_ARRLEN(wcwd));
+        XFILES_ASSERT(_wgetcwd_ret);
+        // https://learn.microsoft.com/en-us/windows/win32/api/stringapiset/nf-stringapiset-widechartomultibyte
+        int WideCharToMultiByte_Return = WideCharToMultiByte(
+            CP_UTF8,
+            WC_ERR_INVALID_CHARS,
+            wcwd,
+            -1,
+            ctx.path_cwd,
+            XFILES_ARRLEN(ctx.path_cwd),
+            NULL,
+            NULL);
+        XFILES_ASSERT(WideCharToMultiByte_Return);
+    }
 #else
     getcwd(ctx.path_cwd, sizeof(ctx.path_cwd));
 #endif
@@ -174,20 +273,17 @@ int main(int argc, char* argv[])
         // TODO verify path is a directory and not a file
 
         // Used for length based string
-        int inlen = strlen(in);
-        if (inlen < 1)
-        {
-            fprintf(stderr, "Invalid path: %s\n", in);
-        }
+        int strlen_ret = strlen(in);
+        XFILES_ASSERT(strlen_ret >= 1);
         // Remove trailing '/' || '\\'
-        if (in[inlen - 1] == XFILES_DIR_CHAR)
-            inlen--;
+        if (in[strlen_ret - 1] == XFILES_DIR_CHAR)
+            strlen_ret--;
 
         int pathlen = 0;
         if (begins_with(in, "./") || begins_with(in, "../"))
             pathlen += snprintf(out + pathlen, sizeof(ctx.path_output) - pathlen, "%s", ctx.path_cwd);
 
-        pathlen += snprintf(out + pathlen, sizeof(ctx.path_output) - pathlen, "%.*s", inlen, in);
+        pathlen += snprintf(out + pathlen, sizeof(ctx.path_output) - pathlen, "%.*s", strlen_ret, in);
     }
     if (ctx.arg_watch == NULL)
         memcpy(ctx.path_watch, ctx.path_cwd, sizeof(ctx.path_cwd));
